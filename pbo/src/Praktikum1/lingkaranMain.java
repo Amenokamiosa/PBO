35f2b637f56462d3775078dc6aae140978387000
Osa <amenokamiosaka@gmail.com>
2025-02-25 09:53:37 +0700

    Add files via upload

---
+++ pbo/src/Praktikum1/lingkaranMain.java
@@ -0,0 +1,8 @@
+package Praktikum1;
+public class lingkaranMain {
+    public static void main(String[] args) {
+        lingkaran luasLingkaran = new lingkaran();
+        luasLingkaran.luas(8);
+    }
+    
+}
