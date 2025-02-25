88fbb51d32c87c820107646a8e57fe646033bfd7
Osa <amenokamiosaka@gmail.com>
2025-02-25 13:52:21 +0700

    Update nilaiFinalMain.java

--- pbo/src/Praktikum1/Mahasiswa/nilaiFinalMain.java
+++ pbo/src/Praktikum1/Mahasiswa/nilaiFinalMain.java
@@ -1,21 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Praktikum1.Mahasiswa;
 
-/**
- *
- * @author admin
- */
+import java.util.Scanner;
+
 public class nilaiFinalMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int uts1, uts2, uas;
+        System.out.print("Masukkan jumlah Mahasiswa: ");
+        int n = input.nextInt();
+        String[] nama = new String[n];  
+        double[] hasilAkhir = new double[n];
+        char[] predikat = new char[n];
+        
+        input.nextLine();  
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nama mahasiswa: ");
+            nama[i] = input.nextLine();  
+
+            System.out.print("Masukkan nilai uts1: ");
+            uts1 = input.nextInt();
+            input.nextLine(); 
+
+            System.out.print("Masukkan nilai uts2: ");
+            uts2 = input.nextInt();
+            input.nextLine(); 
+
+            System.out.print("Masukkan uas: ");
+            uas = input.nextInt();
+            input.nextLine(); 
+
+            hasilAkhir[i] = HitFinal(uts1, uts2, uas);
+            predikat[i] = konversi(hasilAkhir[i]);
+        }
+
+        System.out.println("No. \t Nama Mahasiswa \t Nilai Akhir \t Predikat");
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + 1 + "\t");
+            System.out.print(nama[i] + "\t\t\t");
+            System.out.print(hasilAkhir[i] + "\t\t");
+            System.out.println(predikat[i]);
+        }
     }
-    
+
+    static double HitFinal(double a, double b, double c) {
+        double Final = 0.3 * a + 0.3 * b + 0.4 * c;
+        return Final;
+    }
+
+    static char konversi(double a) {
+        char predikat;
+        if (a >= 80) {
+            predikat = 'A';
+        } else if (a >= 65 && a < 80) {
+            predikat = 'B';
+        } else if (a >= 55 && a < 65) {
+            predikat = 'C';
+        } else if (a >= 45 && a < 55) {
+            predikat = 'D';
+        } else {
+            predikat = 'E';
+        }
+        return predikat;
+    }
 }
