ce0352f60e1f324a1cea5fbbf1b5624ff109fcc2
Osa <amenokamiosaka@gmail.com>
2025-02-25 09:49:52 +0700

    Update Pbo.java

--- pbo/src/pbo/Pbo.java
+++ pbo/src/pbo/Pbo.java
@@ -4,8 +4,6 @@ import Praktikum1.lingkaran;
 
 public class Pbo {
     public static void main(String[] args) {
-        lingkaran luasLingkaran = new lingkaran();
-        luasLingkaran.luas(8);
     }
     
 }
