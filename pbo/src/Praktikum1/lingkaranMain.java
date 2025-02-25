776e454656d974b743b711db7acd98da65a852ce
Osa <amenokamiosaka@gmail.com>
2025-02-25 11:25:05 +0700

    Delete pbo directory

--- pbo/src/Praktikum1/lingkaranMain.java
+++
@@ -1,8 +0,0 @@
-package Praktikum1;
-public class lingkaranMain {
-    public static void main(String[] args) {
-        lingkaran luasLingkaran = new lingkaran();
-        luasLingkaran.luas(8);
-    }
-    
-}
--- pbo/src/pbo/Pbo.java
+++
@@ -1,9 +0,0 @@
-package pbo;
-
-import Praktikum1.lingkaran;
-
-public class Pbo {
-    public static void main(String[] args) {
-    }
-    
-}
